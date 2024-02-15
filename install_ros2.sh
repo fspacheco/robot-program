c82a86de416e6a6f5cdcf14446e295d424987e76
Fernando S. Pacheco <fspacheco@gmail.com>
2024-02-15 13:40:29 +0200

    Add source to bashrc

--- install_ros2.sh
+++ install_ros2.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# Script to install ROS2 (Humble) and Gazebo (Fortress)
+# Script to install ROS2 (Humble), Gazebo (Fortress), Colcon
 #
 # Fernando S. Pacheco
 # Based on https://github.com/ros-industrial/industrial_training/blob/humble/gh_pages/_downloads/ros-industrial-training-setup.sh
@@ -27,9 +27,27 @@ sudo apt install ros-humble-desktop \
     python3-colcon-common-extensions python3-argcomplete \
     ros-humble-pcl-ros pcl-tools \
     python3-rosdep python3-vcstool \
+    python3-pip terminator python-is-python3\
     ros-humble-ros-gz
 
 # rosdep setup
 sudo rosdep init
 rosdep update
 
+# add lines to source needed .sh scripts in .bashrc
+echo "" >> .bashrc
+echo "#Setup for ROS2" >> .bashrc
+echo "source /opt/ros/humble/setup.bash" >> .bashrc
+echo "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" >> .bashrc
+# and now source the file
+source .bashrc
+
+# Need to downgrade setuptools (python pip) 
+# https://learning.oreilly.com/videos/ros2-for-beginners/10000DIVC2022146/10000DIVC2022146-ccccc4/
+pip3 install setuptools==58.2.0
+
+# Directories for ROS workspace
+# Structure from https://learning.oreilly.com/videos/ros2-for-beginners/10000DIVC2022146/10000DIVC2022146-ccccc2/
+mkdir -p ~/ros2_ws/src
+colcon build
+source ~/ros2_ws/install/setup.bash
